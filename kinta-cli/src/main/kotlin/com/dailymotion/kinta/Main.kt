591dfc12f9a233199a84cf920985f944db9bd5fd
Martin Bonnin <martin.bonnin@dailymotion.com>
2020-02-07 16:28:33 +0100

    add 'kinta version'

--- kinta-cli/src/main/kotlin/com/dailymotion/kinta/Main.kt
+++ kinta-cli/src/main/kotlin/com/dailymotion/kinta/Main.kt
@@ -7,13 +7,21 @@ import com.dailymotion.kinta.command.Update
 import com.dailymotion.kinta.integration.gradle.Gradle
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
 import java.io.File
+import java.lang.IllegalStateException
 import java.net.URLClassLoader
 import java.util.*
 import kotlin.system.exitProcess
 
 
 val runtimeCommands by lazy {
+    // Update the project workflows if needed
+    if (File("kintaSrc").exists()) {
+        Gradle(File("kintaSrc")).executeTask("shadowJar")
+    }
+
     val jarFile = File("./kintaSrc/build/libs/kinta-workflows-custom.jar")
 
     val urlClassLoader = URLClassLoader(arrayOf(jarFile.toURI().toURL()))
@@ -35,22 +43,19 @@ val compiledCommands = listOf(
 )
 
 fun main(args: Array<String>) {
-    val hasKintaDir = File("kintaSrc").exists()
-
-    check(hasKintaDir
-            || args.isEmpty()
-            || compiledCommands.map { it.commandName }.contains(args[0])) {
-        "Kinta is not initialized. Please run 'kinta init' at the root of your project"
-    }
-
-    // Update the project workflows if needed
-    if (hasKintaDir) {
-        Gradle(File("kintaSrc")).executeTask("shadowJar")
-    }
-
     val allCommands = runtimeCommands + compiledCommands
-    object : CliktCommand(name = "kinta", help = "mobile workflows automation. Read more at https://dailymotion.github.io/kinta/") {
+    object : CliktCommand(
+            name = "kinta",
+            help = "mobile workflows automation. Read more at https://dailymotion.github.io/kinta/",
+            invokeWithoutSubcommand = true
+    ) {
+        val version by option("--version", "-V").flag()
+
         override fun run() {
+            if (version) {
+                println("$VERSION\n")
+                exitProcess(0)
+            }
         }
     }.subcommands(allCommands.sortedBy { it.commandName })
             .main(args)
