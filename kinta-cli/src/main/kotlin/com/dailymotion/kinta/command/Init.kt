326f503461c5194fc282d7370bd88d0d3558fd9b
Martin Bonnin <martin.bonnin@dailymotion.com>
2020-02-12 00:46:24 +0100

    merge Init and InitCustomWorkflows

--- kinta-cli/src/main/kotlin/com/dailymotion/kinta/command/Init.kt
+++ kinta-cli/src/main/kotlin/com/dailymotion/kinta/command/Init.kt
@@ -1,7 +1,9 @@
 package com.dailymotion.kinta.command
 
 import com.dailymotion.kinta.helper.CommandUtil
+import com.dailymotion.kinta.integration.git.GitIntegration
 import com.dailymotion.kinta.integration.googleplay.internal.PlayStoreInit
+import com.dailymotion.kinta.integration.gradle.Gradle
 import com.github.ajalt.clikt.core.CliktCommand
 import java.io.File
 
@@ -9,7 +11,6 @@ object Init : CliktCommand(name = "init", help = "Initialize a project.") {
     override fun run() {
         File(".kinta").mkdir()
 
-        //Ask for the PLAY API service account
         if (CommandUtil.prompt(
                         message = "Would you like to set up the Play Store config (Useful for GooglePlayIntegration) ?\n" +
                                 "Please refer to https://developers.google.com/android-publisher/getting_started and use a service_account.",
@@ -23,7 +24,47 @@ object Init : CliktCommand(name = "init", help = "Initialize a project.") {
                                 "You can do it later running again this command",
                         options = listOf("yes", "no")) == "yes") {
 
-            InitCustomWorkflows.main(emptyList())
+            createKintaSrc()
+        }
+    }
+
+    private fun createKintaSrc() {
+        check(!File("kintaSrc").exists()) {
+            "There is already a kintaSrc folder in this project. Did you mean to run `kinta update` instead?"
+        }
+
+        File("kintaSrc").mkdirs()
+        copyResource("settings.gradle.kts", "kintaSrc/settings.gradle.kts")
+
+        // Do this early to install the wrapper even if compilation fails
+        // But after setting up settings.gradle.kts
+        Gradle(File("kintaSrc")).executeTask("wrapper")
+
+        File("kintaSrc/src/main/kotlin/com/dailymotion/kinta").mkdirs()
+        File("kintaSrc/src/main/resources/META-INF/services").mkdirs()
+
+        copyResource("CustomWorkflows.kt", "kintaSrc/src/main/kotlin/com/dailymotion/kinta/CustomWorkflows.kt")
+        copyResource("com.dailymotion.kinta.Workflows", "kintaSrc/src/main/resources/META-INF/services/com.dailymotion.kinta.Workflows")
+        copyResource("build.gradle.kts", "kintaSrc/build.gradle.kts")
+
+        File("kintaSrc/build.gradle.kts").apply {
+            writeText(readText().replace("{KINTA_VERSION}", com.dailymotion.kinta.VERSION))
+        }
+        copyResource("gitignore", "kintaSrc/.gitignore")
+        copyResource("kinta.properties", "kinta.properties")
+
+
+        Gradle(File("kintaSrc")).executeTask("assemble")
+        GitIntegration.add(File("kintaSrc").path)
+
+        println("This project is now setup to use kinta. You can define your workflows in kintaSrc/src/main/.... It is meant to be in source control. Take a look around. :)")
+    }
+
+    private fun copyResource(resourceName: String, dirName: String) {
+        javaClass.classLoader.getResourceAsStream(resourceName)!!.use { inputStream ->
+            File(dirName).outputStream().use { outputStream ->
+                inputStream.copyTo(outputStream)
+            }
         }
     }
 }
