f3333a628264cad2d882292b67029412195f7a0b
Martin Bonnin <martin.bonnin@dailymotion.com>
2020-02-11 14:12:31 +0100

    fix ssh-add command

--- scripts/before_script.sh
+++ scripts/before_script.sh
@@ -6,7 +6,7 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
   # switch git to using ssh and a github deploy key so that mkdocs gh-pages can deploy correctly
   openssl aes-256-cbc -K $TRAVIS_KINTA_DEPLOY_KEY_KEY -iv $TRAVIS_KINTA_DEPLOY_KEY_IV -in travis_kinta_deploy_key.enc -out travis_kinta_deploy_key -d
-  ssh-agent add travis_kinta_deploy_key
+  ssh-add travis_kinta_deploy_key
   rm travis_kinta_deploy_key
   git remote set-url origin git@github.com:dailymotion/kinta.git
 fi
