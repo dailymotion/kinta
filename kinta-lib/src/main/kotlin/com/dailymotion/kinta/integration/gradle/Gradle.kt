b7131d0acdf5cca2789934e2c1676c63a4209682
Martin Bonnin <martin.bonnin@dailymotion.com>
2020-02-12 10:58:57 +0100

    Gradle is quiet by default

--- kinta-lib/src/main/kotlin/com/dailymotion/kinta/integration/gradle/Gradle.kt
+++ kinta-lib/src/main/kotlin/com/dailymotion/kinta/integration/gradle/Gradle.kt
@@ -55,7 +55,7 @@ class Gradle(directory: File? = null) {
         val projectConnection = gradleConnector.connect()
         return try {
             var buildLauncher = gradleLambda(projectConnection)
-            buildLauncher = if (Logger.level > Logger.LEVEL_INFO) {
+            buildLauncher = if (Logger.level >= Logger.LEVEL_INFO) {
                 buildLauncher.withArguments("-q")
             } else {
                 buildLauncher.withArguments("--stacktrace")
