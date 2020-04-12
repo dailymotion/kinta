b768f7e333a239d79cc6132588ddc380572a003a
Martin Bonnin <martin.bonnin@dailymotion.com>
2020-04-13 01:24:17 +0200

    add signapk

---
+++ kinta-lib/src/main/kotlin/com/dailymotion/kinta/integration/android/AndroidIntegration.kt
@@ -0,0 +1,76 @@
+package com.dailymotion.kinta.integration.android
+
+import com.dailymotion.kinta.KintaEnv
+import java.io.File
+import java.util.*
+
+@Suppress("NAME_SHADOWING")
+object AndroidIntegration {
+    fun signApk(
+        input: File,
+        output: File,
+        keystore: File? = null,
+        keystorePassword: String? = null,
+        keyAlias: String? = null,
+        keyPassword: String? = null
+    ) {
+        val file = File.createTempFile("keystore", null)
+        try {
+            val keystore = keystore ?: let {
+                file.writeBytes(Base64.getDecoder().decode(KintaEnv.get(KintaEnv.Var.KINTA_KEYSTORE)))
+                file
+            }
+            val keystorePassword = keystorePassword ?: KintaEnv.get(KintaEnv.Var.KINTA_KEYSTORE_PASSWORD)
+            val keyAlias = keyAlias ?: KintaEnv.get(KintaEnv.Var.KINTA_KEY_ALIAS)
+            val keyPassword = keyPassword ?: KintaEnv.get(KintaEnv.Var.KINTA_KEY_PASSWORD)
+
+            val code = ProcessBuilder("which", "apksigner")
+                .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+                .redirectError(ProcessBuilder.Redirect.INHERIT)
+                .start()
+                .waitFor()
+
+            val apksignerCommand = if (code == 0) {
+                "apksigner"
+            } else {
+                val androidHome = System.getenv("ANDROID_HOME")
+
+                check(androidHome != null) {
+                    "make sure apksigner is in your path or that \$ANDROID_HOME is set"
+                }
+                val command = File(androidHome).walkTopDown().firstOrNull {
+                    it.name == "apksigner"
+                }?.absolutePath
+
+                check (command != null) {
+                    "Cannot find apksigner in $androidHome"
+                }
+                command
+            }
+
+            //println("found apksigner at $apksignerCommand")
+
+            ProcessBuilder(
+                apksignerCommand,
+                "sign",
+                "--ks",
+                keystore.absolutePath,
+                "--ks-pass",
+                "pass:$keystorePassword",
+                "--ks-key-alias",
+                keyAlias,
+                "--key-pass",
+                "pass:$keyPassword",
+                "--out",
+                output.absolutePath,
+                input.absolutePath
+            )
+                .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+                .redirectError(ProcessBuilder.Redirect.INHERIT)
+                .start()
+                .waitFor()
+        } finally {
+            file.delete()
+        }
+    }
+}
--- kinta-workflows/src/main/kotlin/com/dailymotion/kinta/workflows/builtin/actions/Actions.kt
+++ kinta-workflows/src/main/kotlin/com/dailymotion/kinta/workflows/builtin/actions/Actions.kt
@@ -5,20 +5,48 @@ import com.dailymotion.kinta.KintaEnv
 import com.dailymotion.kinta.integration.github.GithubIntegration
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
+import java.io.File
 import java.util.*
 
 object ConfigureKeystore : CliktCommand(name = "configureKeystore", help = "configure keystore secrets in github actions. You can later use retrieveKeystore on the CI.") {
     override fun run() {
         val keystore = console.promptFile("Path to keystore:")
-        /*val keystorePassword = console.prompt("Keystore password:")
+        val keystorePassword = console.prompt("Keystore password:")
         val keyAlias = console.prompt("Key alias:")
-        val keyPassword = console.prompt("Key password:")*/
+        val keyPassword = console.prompt("Key password:")
 
         GithubIntegration.setSecret(
             name = KintaEnv.Var.KINTA_KEYSTORE.name,
             value = Base64.getEncoder().encodeToString(keystore.readBytes())
+        )
+        GithubIntegration.setSecret(
+            name = KintaEnv.Var.KINTA_KEYSTORE_PASSWORD.name,
+            value = keystorePassword
+        )
+        GithubIntegration.setSecret(
+            name = KintaEnv.Var.KINTA_KEY_ALIAS.name,
+            value = keyAlias
+        )
+        GithubIntegration.setSecret(
+            name = KintaEnv.Var.KINTA_KEY_PASSWORD.name,
+            value = keyPassword
+        )
+    }
+}
+
+object RetrieveKeystore : CliktCommand(name = "retrieveKeystore", help = "retrieves a keystore that has been set in Github secrets using configureKeystore") {
+    val output by option(help = "path to the keystore file").default("keystore.jks")
+    override fun run() {
+        val keystore = KintaEnv.get(KintaEnv.Var.KINTA_KEYSTORE)
+        check(keystore != null) {
+            "No keystore found. Are you running on Github Actions/Did you callconfigureKeystore first?"
+        }
+
+        File(output).writeBytes(
+            Base64.getDecoder().decode(keystore)
         )
     }
 }
