dbd0c4ab78e39a9e69ce4c7efb311a64b4425c91
Michel Gauzins <michel.gauzins@gmail.com>
2021-05-31 15:35:32 +0200

    Change server name

--- build.gradle.kts
+++ build.gradle.kts
@@ -96,7 +96,7 @@ fun Project.getOssStagingUrl(): String {
         )
     }
     println("publishing to '$repositoryId")
-    return "https://oss.sonatype.org/service/local/staging/deployByRepositoryId/${repositoryId}/".also {
+    return "https://s01.oss.sonatype.org/service/local/staging/deployByRepositoryId/${repositoryId}/".also {
         this.extensions.extraProperties["ossStagingUrl"] = it
     }
 }
