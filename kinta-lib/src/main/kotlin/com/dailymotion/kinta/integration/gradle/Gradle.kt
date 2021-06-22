44f4134b3fc82703658bd2fa804bfd8716e8ec31
Michel Gauzins <michel.gauzins@dailymotion.com>
2021-06-22 09:26:12 +0200

    Support Gradle arguments for Gradle Integration

--- kinta-lib/src/main/kotlin/com/dailymotion/kinta/integration/gradle/Gradle.kt
+++ kinta-lib/src/main/kotlin/com/dailymotion/kinta/integration/gradle/Gradle.kt
@@ -15,15 +15,14 @@ class Gradle(directory: File? = null) {
             .forProjectDirectory(directory ?: File("."))
 
 
-    fun executeTask(task: String) = executeTasks(task)
+    fun executeTask(task: String, args: List<String> = emptyList()) = executeTasks(tasks = arrayOf(task), args = args)
 
-    fun executeTasks(vararg tasks: String) = runGradleBuild { projectConnection ->
-        logExecuteTasks(*tasks)
-        projectConnection.newBuild().forTasks(*tasks)
-
+    fun executeTasks(vararg tasks: String, args: List<String> = emptyList()) = runGradleBuild { projectConnection ->
+        logExecuteTasks(*tasks, args = args)
+        projectConnection.newBuild().forTasks(*tasks).withArguments(args)
     }
 
-    fun executeGroupTasks(groupName: String, vararg tasks: String) = runGradleBuild { projectConnection ->
+    fun executeGroupTasks(groupName: String, vararg tasks: String, args: List<String> = emptyList()) = runGradleBuild { projectConnection ->
         val rootProjectModel = projectConnection.getModel(GradleProject::class.java)
 
         val taskList = mutableListOf(*tasks)
@@ -32,23 +31,29 @@ class Gradle(directory: File? = null) {
             val subProjectGroupTaskList = subProject.tasks.filter { it.group == groupName }.map { it.name }
             taskList.addAll(subProjectGroupTaskList)
         }
-        logExecuteGroupTasks(groupName, taskList.subList(tasks.size, taskList.size))
-        projectConnection.newBuild().forTasks(*taskList.toTypedArray())
+        logExecuteGroupTasks(groupName, taskList.subList(tasks.size, taskList.size), args = args)
+        projectConnection.newBuild().forTasks(*taskList.toTypedArray()).withArguments(args)
     }
 
-    private fun logExecuteTasks(vararg taskName: String) {
+    private fun logExecuteTasks(vararg taskName: String, args: List<String> = emptyList()) {
         taskName.forEach {
-            Logger.i("\n==> Executing task: $it\n")
+            Logger.i("\n==> Executing task: $it")
+        }
+        if(args.isNotEmpty()) {
+            Logger.i(args.joinToString(separator = ", "))
         }
     }
 
-    private fun logExecuteGroupTasks(groupName: String, taskList: List<String>) {
+    private fun logExecuteGroupTasks(groupName: String, taskList: List<String>, args: List<String> = emptyList()) {
         Logger.i("\n==> List of task to execute for group: $groupName")
         Logger.i("==> {")
         taskList.forEach {
             Logger.i("==>\t${it}")
         }
-        Logger.i("==> }\n")
+        Logger.i("==> }")
+        if(args.isNotEmpty()) {
+            Logger.i(args.joinToString(separator = ", "))
+        }
     }
 
     private fun runGradleBuild(gradleLambda: (ProjectConnection) -> BuildLauncher): Int {
@@ -56,9 +61,9 @@ class Gradle(directory: File? = null) {
         return try {
             var buildLauncher = gradleLambda(projectConnection)
             buildLauncher = if (Logger.level >= Logger.LEVEL_INFO) {
-                buildLauncher.withArguments("-q")
+                buildLauncher.addArguments("-q")
             } else {
-                buildLauncher.withArguments("--stacktrace")
+                buildLauncher.addArguments("--stacktrace")
             }
 
             buildLauncher
