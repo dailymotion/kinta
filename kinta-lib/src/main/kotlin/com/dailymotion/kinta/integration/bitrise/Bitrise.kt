ca15a9e292c286f9bf3c113ccc8c8fdda8b79871
m.gauzins <michel.gauzins@dailymotion.com>
2025-02-11 13:46:52 +0100

    Bitrise artefact capabilities

--- kinta-lib/src/main/kotlin/com/dailymotion/kinta/integration/bitrise/Bitrise.kt
+++ kinta-lib/src/main/kotlin/com/dailymotion/kinta/integration/bitrise/Bitrise.kt
@@ -116,6 +116,95 @@ object Bitrise {
         throw Exception("error parsing workflows list: ${response.code}: ${response.body!!.string()}")
     }
 
+    @OptIn(ExperimentalSerializationApi::class)
+    fun getBuild(
+        token: String? = null,
+        repoName: String,
+        buildSlug: String,
+    ): Build {
+
+        val token_ = token ?: KintaEnv.getOrFail(KintaEnv.Var.BITRISE_PERSONAL_TOKEN)
+
+        val appSlug = findAppSlug(token_, repoName)
+
+        val request = Request.Builder()
+            .header("Authorization", "token $token_")
+            .url("$BITRISE_API_URL/$appSlug/builds/$buildSlug")
+            .get()
+            .build()
+
+        val response = OkHttpClient().newCall(request).execute()
+
+        if (!response.isSuccessful) {
+            throw Exception("cannot get build: ${response.code}: ${response.body!!.string()}")
+        }
+
+        response.body?.string()?.let { data ->
+            val node: BuildResponse = json.decodeFromString(data)
+            return node.data
+        }
+        throw Exception("error parsing build: ${response.code}: ${response.body!!.string()}")
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    fun getArtifactsFromBuild(
+        token: String? = null,
+        repoName: String,
+        buildSlug: String,
+    ): List<Artifact> {
+
+        val token_ = token ?: KintaEnv.getOrFail(KintaEnv.Var.BITRISE_PERSONAL_TOKEN)
+
+        val appSlug = findAppSlug(token_, repoName)
+        val request = Request.Builder()
+            .header("Authorization", "token $token_")
+            .url("$BITRISE_API_URL/$appSlug/builds/$buildSlug/artifacts")
+            .get()
+            .build()
+
+        val response = OkHttpClient().newCall(request).execute()
+
+        if (!response.isSuccessful) {
+            throw Exception("cannot get artifacts list: ${response.code}: ${response.body!!.string()}")
+        }
+
+        response.body?.string()?.let { data ->
+            val node: ArtifactsResponse = json.decodeFromString(data)
+            return node.data
+        }
+        throw Exception("error parsing artifacts list: ${response.code}: ${response.body!!.string()}")
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    fun getArtifactDetail(
+        token: String? = null,
+        repoName: String,
+        buildSlug: String,
+        artifactSlug: String,
+    ): ArtifactDetail {
+
+        val token_ = token ?: KintaEnv.getOrFail(KintaEnv.Var.BITRISE_PERSONAL_TOKEN)
+
+        val appSlug = findAppSlug(token_, repoName)
+        val request = Request.Builder()
+            .header("Authorization", "token $token_")
+            .url("$BITRISE_API_URL/$appSlug/builds/$buildSlug/artifacts/$artifactSlug")
+            .get()
+            .build()
+
+        val response = OkHttpClient().newCall(request).execute()
+
+        if (!response.isSuccessful) {
+            throw Exception("cannot get artifact detail: ${response.code}: ${response.body!!.string()}")
+        }
+
+        response.body?.string()?.let { data ->
+            val node: ArtifactDetailResponse = json.decodeFromString(data)
+            return node.data
+        }
+        throw Exception("error parsing artifact detail: ${response.code}: ${response.body!!.string()}")
+    }
+
     @Serializable
     private data class WorkflowsResponse(val data: List<String>)
 
@@ -124,4 +213,47 @@ object Bitrise {
 
     @Serializable
     private data class AppItem(val slug: String, val repo_slug: String)
+
+    @Serializable
+    private data class BuildResponse(val data: Build)
+
+    @Serializable
+    data class Build(
+        val slug: String,
+        val triggered_at: String,
+        val status: Int,
+        val is_processed: Boolean,
+        val branch: String?,
+        val build_number: Int,
+        val commit_hash: String?,
+        val tag: String?,
+        val triggered_workflow: String,
+        val pull_request_id: Int?,
+    )
+
+    @Serializable
+    private data class ArtifactsResponse(val data: List<Artifact>)
+
+    @Serializable
+    data class Artifact(
+        val title: String,
+        val artifact_type: String,
+        val is_public_page_enabled: Boolean,
+        val slug: String,
+        val file_size_bytes: Long,
+    )
+
+    @Serializable
+    private data class ArtifactDetailResponse(val data: ArtifactDetail)
+
+    @Serializable
+    data class ArtifactDetail(
+        val title: String,
+        val slug: String,
+        val artifact_type: String,
+        val expiring_download_url: String,
+        val is_public_page_enabled: Boolean,
+        val public_install_page_url: String,
+        val file_size_bytes: Long,
+    )
 }
